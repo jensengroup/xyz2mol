9b7bd4d876162c65c722792924fb67125d38dd49
Jimmy Charnley Kromann <jimmy@charnley.dk>
2018-06-25 16:07:22 +0200

    removed test

--- xyz2mol.py
+++ xyz2mol.py
@@ -349,56 +349,17 @@ if __name__ == "__main__":
     parser.add_argument('structure', metavar='structure', type=str)
     args = parser.parse_args()
 
-    if args.structure == "test":
+    filename = args.structure
+    charged_fragments = True
+    atomicNumList,charge,xyz_coordinates = read_xyz_file(filename)
 
-        filename = "ethane.xyz"
-        filename = "acetate.xyz"
-    
-        charged_fragments = True
+    mol = xyz2mol(atomicNumList,charge,xyz_coordinates,charged_fragments)
 
-        atomicNumList,charge,xyz_coordinates = read_xyz_file(filename)
-        mol = xyz2mol(atomicNumList,charge,xyz_coordinates,charged_fragments)
+    # Canonical hack
+    smiles = Chem.MolToSmiles(mol)
+    m = Chem.MolFromSmiles(smiles)
+    smiles = Chem.MolToSmiles(m)
 
-        print Chem.MolToSmiles(mol)
-
-        # code to test using SMILES instead of xyz file
-        smiles_list = ['C=C([O-])CC','C=C([NH3+])CC','CC(=O)[O-]','C[N+](=O)[O-]','CS(CC)(=O)=O','CS([O-])(=O)=O',
-                    'C=C(C)CC', 'CC(C)CC','C=C(N)CC','C=C(C)C=C','C#CC=C','c1ccccc1','c1ccccc1c1ccccc1',
-                    '[NH3+]CS([O-])(=O)=O','CC(NC)=O','C[NH+]=C([O-])CC[NH+]=C([O-])C','C[NH+]=CC=C([O-])C',
-                    "[C+](C)(C)CC[C-](C)(C)","[CH2][CH2][CH]=[CH][CH2]"]
-
-        for smiles in smiles_list:
-            mol = Chem.MolFromSmiles(smiles)
-
-            rdmolops.Kekulize(mol, clearAromaticFlags = True)
-            charge = Chem.GetFormalCharge(mol)
-            mol = Chem.AddHs(mol)
-            atomicNumList = [a.GetAtomicNum() for a in mol.GetAtoms()]
-            proto_mol = get_proto_mol(atomicNumList)
-
-            AC = Chem.GetAdjacencyMatrix(mol)
-
-            newmol = AC2mol(proto_mol,AC,atomicNumList,charge,charged_fragments)
-            newmol = Chem.RemoveHs(newmol)
-            newmol_smiles = Chem.MolToSmiles(newmol)
-            
-            mol = Chem.RemoveHs(mol)
-            canonical_smiles = Chem.MolToSmiles(mol)
-            if newmol_smiles != canonical_smiles:
-                print "uh,oh", smiles, newmol_smiles
-
-    else:
-        filename = args.structure
-        charged_fragments = True
-        atomicNumList,charge,xyz_coordinates = read_xyz_file(filename)
-   
-        mol = xyz2mol(atomicNumList,charge,xyz_coordinates,charged_fragments)
-
-        # Canonical hack
-        smiles = Chem.MolToSmiles(mol)
-        m = Chem.MolFromSmiles(smiles)
-        smiles = Chem.MolToSmiles(m)
-
-        print smiles
+    print smiles
 
 
