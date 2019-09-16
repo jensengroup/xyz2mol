7142dc30f09d3c88992d3cb90fc0a7bfa0b0b514
Jimmy Charnley Kromann <jimmy@charnley.dk>
2019-09-16 14:47:06 +0200

    proper testing using pytest

--- test.py
+++ test.py
@@ -1,62 +1,168 @@
+
+import pytest
+import rmsd
+import numpy as np
+
 from rdkit import Chem
 from rdkit.Chem import rdmolops
 from rdkit.Chem import AllChem
+
 import xyz2mol as x2m
 
+__TEST_SMILES__ = [
+    'C=C([O-])CC',
+    'C=C([NH3+])CC',
+    'CC(=O)[O-]',
+    'C[N+](=O)[O-]',
+    'CS(CC)(=O)=O',
+    'CS([O-])(=O)=O',
+    'C=C(C)CC',
+    'CC(C)CC',
+    'C=C(N)CC',
+    'C=C(C)C=C',
+    'C#CC=C',
+    'c1ccccc1',
+    'c1ccccc1c1ccccc1',
+    '[NH3+]CS([O-])(=O)=O',
+    'CC(NC)=O',
+    '[O-]c1ccccc1',
+    'O=C(C=C1)C=CC1=CCC([O-])=O',
+    'C#CC#C',
+    'Cc1ccc(cc1)C1C=CC2C(C=CC2(C#N)C#N)=CC=1'
+]
 
-if __name__ == "__main__":
+__TEST_FILES__ = [
+    ("examples/ethane.xyz", 0, "CC"),
+    ("examples/acetate.xyz", -1, "CC(=O)[O-]"),
+    ("examples/chiral_stereo_test.xyz", 0, "C/C=C/[C@@H](C)F"),
+]
 
-    filename = "ethane.xyz"
-    filename = "acetate.xyz"
-    filename = "chiral_stereo_test.xyz"
+def get_atoms(mol):
+    atoms = [a.GetAtomicNum() for a in mol.GetAtoms()]
+    return atoms
+
+def get_mol(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    Chem.Kekulize(mol, clearAromaticFlags=True)
+    charge = Chem.GetFormalCharge(mol)
+    mol = Chem.AddHs(mol)
+    return mol
+
+def generate_structure_from_smiles(smiles):
+
+    # Generate a 3D structure from smiles
+
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+
+    status = AllChem.EmbedMolecule(mol)
+    status = AllChem.UFFOptimizeMolecule(mol)
+
+    conformer = mol.GetConformer()
+    coordinates = conformer.GetPositions()
+    coordinates = np.array(coordinates)
+
+    atoms = get_atoms(mol)
+
+    return atoms, coordinates
+
+def get_mol_skeleton(mol):
+
+    atoms = get_atoms(mol)
+    adjacent_matrix = Chem.GetAdjacencyMatrix(mol)
+
+    return proto_mol, adjacent_matrix
+
+@pytest.mark.parametrize("smiles", __TEST_SMILES__)
+def test_smiles_from_adjacent_matrix(smiles):
 
     charged_fragments = True
     quick = True
 
-    atomicNumList,charge,xyz_coordinates = x2m.read_xyz_file(filename)
-    mol = x2m.xyz2mol(atomicNumList,charge,xyz_coordinates,charged_fragments,quick)
+    # Cut apart the smiles
+    mol = get_mol(smiles)
+    atoms = get_atoms(mol)
+    charge = Chem.GetFormalCharge(mol)
+    adjacent_matrix = Chem.GetAdjacencyMatrix(mol)
 
-    print(Chem.MolToSmiles(mol, isomericSmiles=True))
+    #
+    mol = Chem.RemoveHs(mol)
+    canonical_smiles = Chem.MolToSmiles(mol)
 
-    # code to test using SMILES instead of xyz file
-    smiles_list = ['C=C([O-])CC','C=C([NH3+])CC','CC(=O)[O-]','C[N+](=O)[O-]','CS(CC)(=O)=O','CS([O-])(=O)=O',
-                'C=C(C)CC', 'CC(C)CC','C=C(N)CC','C=C(C)C=C','C#CC=C','c1ccccc1','c1ccccc1c1ccccc1',
-                '[NH3+]CS([O-])(=O)=O','CC(NC)=O','[O-]c1ccccc1','O=C(C=C1)C=CC1=CCC([O-])=O',
-                'C#CC#C','Cc1ccc(cc1)C1C=CC2C(C=CC2(C#N)C#N)=CC=1']
-    #smiles_list = ['C[NH+]=C([O-])CC[NH+]=C([O-])C','C[NH+]=CC=C([O-])C',
-    #            "[C+](C)(C)CC[C-](C)(C)",'O=C(C=C1)C=CC1=CCC([O-])=O',
-    #            'O=C([CH-]C=CC(C([O-])=O)=O)[O-]','[O-]c1ccccc1','CNC(C(C)=[NH+][CH-]CC(O)=O)=O',"[CH2][CH2][CH]=[CH][CH2]"]
-    #smiles_list = ['Cc1ccc(cc1)C1C=CC2C(C=CC2(C#N)C#N)=CC=1']
-    #smiles_list = ['CC1C=CC2C(C=CC2(C)C)=CC=1']
-    #smiles_list = ['CC1=CC=C(C=CC2)C2C=C1']
-    #smiles_list = ['CC1=CC=C(C2=CC=CC=C2)C=C1']
-    #smiles_list = ['C1(CC2=CC=CC=C2)=CC=CC=C1']
-    #smiles_list = ['[O-]c1ccccc1[O-]']
-    #smiles_list = ['C[N+](=O)[O-]']
-    #smiles_list = ['N#CC(C#N)=CC=C1C=CC=CC(=C1)c1ccc(cc1)[N+](=O)[O-]']
-    #smiles_list = ['CNC([O-])=C([NH+]=C/CC(O)=O)C']
+    # Define new molecule template from atoms
+    new_mol = x2m.get_proto_mol(atoms)
 
-    for smiles in smiles_list:
-        #print(smiles)
-        mol = Chem.MolFromSmiles(smiles)
+    # reconstruct the molecule from adjacent matrix, atoms and total charge
+    new_mol = x2m.AC2mol(new_mol, adjacent_matrix, atoms, charge, charged_fragments, quick)
+    new_mol = Chem.RemoveHs(new_mol)
+    new_mol_smiles = Chem.MolToSmiles(new_mol)
 
-        Chem.Kekulize(mol, clearAromaticFlags = True)
-        charge = Chem.GetFormalCharge(mol)
-        mol = Chem.AddHs(mol)
-        atomicNumList = [a.GetAtomicNum() for a in mol.GetAtoms()]
-        proto_mol = x2m.get_proto_mol(atomicNumList)
+    assert new_mol_smiles == canonical_smiles
 
-        AC = Chem.GetAdjacencyMatrix(mol)
+    return
 
-        newmol = x2m.AC2mol(proto_mol,AC,atomicNumList,charge,charged_fragments,quick)
-        #print Chem.MolToSmiles(newmol)
+@pytest.mark.parametrize("smiles", __TEST_SMILES__)
+def test_smiles_from_coord(smiles):
 
-        newmol = Chem.RemoveHs(newmol)
-        newmol_smiles = Chem.MolToSmiles(newmol)
-        #print (newmol_smiles)
+    charged_fragments = True
+    quick = True
 
-        mol = Chem.RemoveHs(mol)
-        canonical_smiles = Chem.MolToSmiles(mol)
-        if newmol_smiles != canonical_smiles:
-            print("uh,oh", smiles, newmol_smiles)
+    # The answer
+    mol = Chem.MolFromSmiles(smiles)
+    charge = Chem.GetFormalCharge(mol)
+    canonical_smiles = Chem.MolToSmiles(mol, isomericSmiles=False)
+
+    # generate forcefield coordinates
+    atoms, coordinates = generate_structure_from_smiles(smiles)
+
+    # Generate molobj from atoms, charge and coordinates
+    mol = x2m.xyz2mol(atoms, charge, coordinates, charged_fragments, quick)
+
+    # For this test, remove chira. clean and canonical
+    Chem.Kekulize(mol)
+    mol = Chem.RemoveHs(mol)
+    Chem.RemoveStereochemistry(mol)
+    smiles = Chem.MolToSmiles(mol, isomericSmiles=False)
+
+    # Please look away. A small hack that removes the explicit hydrogens
+    mol = Chem.MolFromSmiles(smiles)
+    smiles = Chem.MolToSmiles(mol)
+
+    assert smiles == canonical_smiles
+
+    return
+
+
+@pytest.mark.parametrize("filename, charge, answer", __TEST_FILES__)
+def test_smiles_from_xyz_files(filename, charge, answer):
+
+    charged_fragments = True
+    quick = True
+
+    atoms, coordinates = rmsd.get_coordinates_xyz(filename)
+    atoms = [x2m.int_atom(atom) for atom in atoms]
+
+    mol = x2m.xyz2mol(atoms, charge, coordinates, charged_fragments, quick)
+    mol = Chem.RemoveHs(mol)
+
+    smiles = Chem.MolToSmiles(mol)
+
+    assert smiles == answer
+
+    return
+
+
+if __name__ == "__main__":
+
+    for smiles in __TEST_SMILES__:
+        test_smiles_from_adjacent_matrix(smiles)
+        print(True, smiles)
+
+    for filename, charge, answer in __TEST_FILES__:
+        test_smiles_from_xyz_files(filename, charge, answer)
+        print(True, answer)
+
+    for smiles in __TEST_SMILES__:
+        test_smiles_from_coord(smiles)
+        print(True, smiles)
 
