b86529f15f7a9fc728002adb17739c930c5f8bce
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-01-24 19:56:17 +0100

    install xyz2mol as bin

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     description="Convert Cartesian coordinates to one or more molecular graphs",
     url="https://github.com/jensengroup/xyz2mol",
     py_modules=["xyz2mol"],
+    entry_points={"console_scripts": ["xyz2mol=xyz2mol:main"]},
 )
