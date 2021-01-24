c56329b75bd6d35077a4f14b00ced7ae648c9280
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-01-24 17:09:31 +0100

    init setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="xyz2mol",
+    version="1.0",
+    description="Convert Cartesian coordinates to one or more molecular graphs",
+    url="https://github.com/jensengroup/xyz2mol",
+)
